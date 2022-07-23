9fb86d8ce77eda2d1427cc61d9e51da0a8597616
unknown <abdallamfathy@gmail.com>
2022-07-23 17:20:09 +0200

    edit tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "public/index.html",
+    "index.html",
     "public/indexx.js",
     "./node_modules/flowbite/**/*.js",
     
